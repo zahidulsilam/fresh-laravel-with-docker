d1bd431260d643a3b39ffc4b1a6816ded277ff47
zahidulsilam <zahidislam08@gmail.com>
2026-02-09 04:31:56 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -45,7 +45,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('MYSQLHOST', '127.0.0.1'),
+            'host' => env('MYSQLHOST', 'db'),
             'port' => env('MYSQLPORT', '3306'),
             'database' => env('MYSQLDATABASE', 'laravel'),
             'username' => env('MYSQLUSER', 'root'),
